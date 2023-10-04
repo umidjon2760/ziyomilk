6479676780e5a376c7ff9cc003972f2465954d16
Umidjon Ahmadjonov <u.ahmadjonov@hamkorbank.uz>
2023-10-04 17:36:42 +0500

    sidebar green

--- views/layouts/sidebar.php
+++ views/layouts/sidebar.php
@@ -1,4 +1,4 @@
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
+<aside class="main-sidebar sidebar-dark-success elevation-4">
     <!-- Brand Logo -->
     <a href="<?= \yii\helpers\Url::home() ?>" class="brand-link">
         <img src="<?= $assetDir ?>/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
