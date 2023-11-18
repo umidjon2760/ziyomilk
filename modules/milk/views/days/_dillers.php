5ade72165c58fbcca94474928bc511b34a814400
umidjon2760 <umidaxmadjonov2263@gmail.com>
2023-11-18 23:20:58 +0500

    dsa

--- modules/milk/views/days/_dillers.php
+++ modules/milk/views/days/_dillers.php
@@ -10,7 +10,9 @@
         echo "<tr>";
         echo "<th style='width:2%;' class='hor-center ver-middle'>#</th>";
         echo "<th  class='hor-center ver-middle'>Diller</th>";
+        $td_count = 0;
         foreach ($products as $product) {
+            $td_count++;
             echo "<th style='width:4%;' class='hor-center ver-middle'>" . $product->name . "</th>";
         }
         echo "<th style='width:4%;' class='hor-center ver-middle'>Bergan summa</th>";
@@ -19,20 +21,27 @@
         echo "<th style='width:4%;' class='hor-center ver-middle'>Ko'rish</th>";
         echo "</tr>";
         $n = 1;
+        $all_given_sum = 0;
+        $all_loan_sum = 0;
+        $all_old_loan_sum = 0;
+        $all_all_sum = 0;
         foreach ($dillers as $diller) {
             $dillers_calc = $diller->getDillerCalcByDay($model->day);
-            if($dillers_calc){
+            if ($dillers_calc) {
                 $given_sum = $dillers_calc->given_sum;
                 $loan_sum = $dillers_calc->loan_sum;
                 $old_loan_sum = $dillers_calc->old_loan_sum;
                 $all_sum = $dillers_calc->all_sum;
-            }
-            else{
+            } else {
                 $given_sum = 0;
                 $loan_sum = 0;
                 $old_loan_sum = 0;
                 $all_sum = 0;
             }
+            $all_given_sum += $given_sum;
+            $all_loan_sum += $loan_sum;
+            $all_old_loan_sum += $old_loan_sum;
+            $all_all_sum += $all_sum;
             echo "<tr>";
             echo "<td class='hor-center ver-middle'>" . $n . "</td>";
             echo "<td class='ver-middle'>" . $diller->name . "</td>";
@@ -42,13 +51,21 @@
                 echo $selling ? $selling->buy - $selling->return : 0;
                 echo "</td>";
             }
-            echo "<td class='hor-center ver-middle'>" . numberFormat($given_sum,0) . "</td>";
-            echo "<td class='hor-center ver-middle'>" . numberFormat($loan_sum,0). "<br>".numberFormat($old_loan_sum,0) . "</td>";
-            echo "<td class='hor-center ver-middle'>" . numberFormat($all_sum,0) . "</td>";
+            echo "<td class='hor-center ver-middle'>" . numberFormat($given_sum, 0) . "</td>";
+            echo "<td class='hor-center ver-middle'>" . numberFormat($loan_sum, 0) . "<br>" . numberFormat($old_loan_sum, 0) . "</td>";
+            echo "<td class='hor-center ver-middle'>" . numberFormat($all_sum, 0) . "</td>";
             echo "<td style='width:5%;' class='hor-center ver-middle'><a href='?r=milk/days/diller-view&id=" . $diller->id . "&day_id=" . $model->id . "' class='btn btn-sm btn-info'>Ko'rish</a></td>";
             echo "</tr>";
             $n++;
         }
+        $colspan = $td_count + 2;
+        echo "<tr>";
+        echo "<td colspan='" . $colspan . "' class='text-right'><b>Jami</b></td>";
+        echo "<td class='hor-center ver-middle'>" . numberFormat($all_given_sum, 0) . "</td>";
+        echo "<td class='hor-center ver-middle'>" . numberFormat($all_loan_sum, 0) . "<br>" . numberFormat($all_old_loan_sum, 0) . "</td>";
+        echo "<td class='hor-center ver-middle'>" . numberFormat($all_all_sum, 0) . "</td>";
+        echo "<td></td>";
+        echo "</tr>";
         echo "</table>";
         ?>
     </div>
